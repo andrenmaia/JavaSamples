6861b650ba9ee60d7e347d896d22eec8723e7350
Caio Maia <caionmaia@outlook.com>
2015-01-31 08:07:38 -0800

    Add singleton messages

--- hello_spring_bean_metadata/src/main/java/com/company/app/App.java
+++ hello_spring_bean_metadata/src/main/java/com/company/app/App.java
@@ -12,8 +12,20 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld helloWorld = (HelloWorld)context.getBean("helloWorld");
 
-        System.out.println(helloWorld.getMessage());
+        HelloWorld helloWorld = (HelloWorld)context.getBean("helloWorld");
+        HelloWorld helloWorld2 = (HelloWorld)context.getBean("helloWorld");
+
+
+        System.out.println("Original message:" + helloWorld.getMessage());
+
+
+        helloWorld2.setMessage("New message helloWorld!");
+        System.out.println("Message changed.");
+
+        // Check is if singleton and print messages.
+        System.out.println("From helloWorld:" + helloWorld.getMessage());
+        System.out.println("From helloWorld2:" + helloWorld2.getMessage());
+        System.out.println("Is singleton:" + helloWorld.getMessage().equals(helloWorld2.getMessage()));
     }
 }
